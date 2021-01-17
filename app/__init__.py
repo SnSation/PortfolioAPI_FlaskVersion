151d986706b2d30d609b4a3ee535fe12839dbca2
SnSation <blair95273@gmail.com>
2021-01-16 23:26:07 -0600

    cors attempt 1

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,11 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_cors import CORS, cross_origin
 
 db = SQLAlchemy()
 migrate = Migrate()
+cors = CORS()
 
 def create_app(config=Config):
     app = Flask(__name__)
@@ -12,6 +14,8 @@ def create_app(config=Config):
 
     db.init_app(app)
     migrate.init_app(app, db)
+    cors.init_app(app)
+    app.config['CORS_HEADERS'] = 'Content-Type'
 
     from app.blueprints.home import bp as home_bp
     app.register_blueprint(home_bp)
